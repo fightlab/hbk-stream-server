e910e8924b0bc2021d4546543c8f53f9bc333d0e
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2018-07-03 10:14:03 +0100

    test

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ io.on('connection', socket => {
   console.log('a user connected');
 });
 
+setTimeout(() => io.emit('date', new Date().getTime()))
+
 http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
