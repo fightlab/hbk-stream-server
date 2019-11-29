96c1fee7710ea41bb63b53c028de5bbb30702526
Mahesh Makani <maheshmakani@gmail.com>
2019-11-29 13:41:42 +0000

    typescript setup

--- backpack.config.js
+++ backpack.config.js
@@ -1,12 +1,20 @@
+ 
 module.exports = {
   webpack: (config, options, webpack) => {
-    // Perform customizations to config
-    // Important: return the modified config
-
-    // changes the name of the entry point from index -> main.js
     config.entry.main = [
-      './index.js'
+      './src/index.ts'
     ]
+
+    config.resolve = {
+      extensions: [".ts", ".js", ".json"]
+    };
+
+    config.module.rules.push(
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    );
 
     return config
   }
